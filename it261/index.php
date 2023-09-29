df4541cfd365d87d578abd5facd03f79fb8fd3ac
guoweikong <124006367+guoweikong@users.noreply.github.com>
2023-09-29 15:24:32 -0700

    Add files via upload

---
+++ it261/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portal Page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+    <h1><a href="index.php">Guowei's Portal Page</a></h1>
+    <h2>The navigation below represents our BIG Assignments.</h2>
+    <div id="wrapper">
+        <nav>
+            <ul>
+                <li><a href="">Switch</a></li>
+                <li><a href="">Troubleshoot</a></li>
+                <li><a href="">Calculator</a></li>
+                <li><a href="">Email</a></li>
+                <li><a href="">Database</a></li>
+                <li><a href="">Gallery</a></li>
+            </ul>
+        </nav>
+
+        <main>
+            <h2>About Guowei</h2>
+            <img class="right" src="images/guowei.jpg" alt="guowei">
+            <p>A paragraph about myself. i.e., goals, background, hobbies etc. Integer egestas odio vulputate lacus vestibulum ultricies. Vestibulum odio lacus, sodales a accumsan quis, mollis quis diam. Proin lobortis sit amet mauris ac finibus. Nulla dignissim aliquam urna, quis cursus purus lacinia placerat. Donec et mauris at lacus pulvinar tempus. Nulla id risus in eros lobortis dapibus ac at neque. Aenean varius urna fringilla ante commodo consequat. Donec vel nisl nulla.</p>
+            <img src="images/screenshot.png" alt="screenshot">
+        </main>
+
+        <aside>
+            <h2>Weekly Class Exercises</h2>
+            <h3>Week 2</h3>
+                <ol>
+                    <li><a href="var.php">var.php</a></li>
+                    <li><a href="">var2.php</a></li>
+                    <li><a href="">3.php</a></li>
+                    <li><a href="">4.php</a></li>
+                </ol>
+        </aside>
+
+    </div>
+    <!-- close wrapper -->
+
+</body>
+</html>
