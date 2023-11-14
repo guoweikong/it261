73986631822d43cc1184bcb4e626fcf50c4f6fb6
guoweikong <124006367+guoweikong@users.noreply.github.com>
2023-11-13 16:16:34 -0800

    week7 assignment

--- it261/weeks/week7/pictures.php
+++ it261/weeks/week7/pictures.php
@@ -1,16 +1,27 @@
 <?php
 
 $people['Donald_Trump'] = 'trump_Former President from NY.';
+$people['Donald_Trump'] = 'trump2_Former President from NY.';
 $people['Joe_Biden'] = 'biden_President from PA.';
+$people['Joe_Biden'] = 'biden2_President from PA.';
 $people['Hilary_Clinton'] = 'clint_Secretary from NY.';
+$people['Hilary_Clinton'] = 'clint2_Secretary from NY.';
 $people['Bernie_Sanders'] = 'sande_Senator from VT.';
+$people['Bernie_Sanders'] = 'sande2_Senator from VT.';
 $people['Elizabeth_Warren'] = 'warre_Senator from MA.';
+$people['Elizabeth_Warren'] = 'warre2_Senator from MA.';
 $people['Kamala_Harris'] = 'harri_Vice President from CA.';
+$people['Kamala_Harris'] = 'harri2_Vice President from CA.';
 $people['Cory_Booker'] = 'booke_Senator from NJ.';
+$people['Cory_Booker'] = 'booke2_Senator from NJ.';
 $people['Andrew_Yang'] = 'ayang_Entrepreneur from NY.';
+$people['Andrew_Yang'] = 'ayang2_Entrepreneur from NY.';
 $people['Pete_Buttigieg'] = 'butti_Transportation Secretary from IN.';
+$people['Pete_Buttigieg'] = 'butti2_Transportation Secretary from IN.';
 $people['Amy_Klobuchar'] = 'klobu_Senator from MN.';
+$people['Amy_Klobuchar'] = 'klobu2_Senator from MN.';
 $people['Julian_Castro'] = 'castr_Former Housing/Urban from TX.';
+$people['Julian_Castro'] = 'castr2_Former Housing/Urban from TX.';
 // variable     key     =     value
 //  $name                     $image
 
@@ -41,6 +52,7 @@ $people['Julian_Castro'] = 'castr_Former Housing/Urban from TX.';
 <td><img src="images/<?php echo substr($image, 0, 5) ;?>.jpg" alt="<?php echo str_replace('_', ' ', $name)  ;?>"></td>
 <td><?php echo str_replace('_', ' ', $name)  ;?></td>
 <td><?php echo substr($image, 6)  ;?></td>
+<td><img src="images/<?php echo substr($image, 0, 6) ;?>.jpg" alt="<?php echo str_replace('_', ' ', $name)  ;?>"></td>
 </tr>
 <?php  endforeach  ; ?>
 </table>    
