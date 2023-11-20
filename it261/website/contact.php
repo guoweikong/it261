9a386ca005462bbcf07219ab88d4f9d4068181a3
guoweikong <124006367+guoweikong@users.noreply.github.com>
2023-11-20 10:26:00 -0800

    gallery.php

---
+++ it261/website/contact.php
@@ -0,0 +1,28 @@
+<?php 
+include('config.php');
+include('./includes/header.php');
+?>
+
+
+
+<div id="wrapper2">
+
+
+
+    <h1 class="contact">We'd love to hear from you!</h1>
+    
+    
+
+    <?php
+    include('./includes/form.php') ;
+    ?>
+
+
+
+
+
+</div>
+<!-- end wrapper -->
+
+<?php
+include('./includes/footer.php');
---
+++ it261/website/thx.php
@@ -0,0 +1,20 @@
+<?php 
+include('config.php');
+include('./includes/header.php');
+?>
+
+
+
+<div id="wrapper">
+
+<img class="thx" src="images/thx.png" alt="thanks"> 
+
+<div class="back-to-home">
+    <a href="index.php">Back to Home</a>
+  </div>
+
+</div>
+<!-- end wrapper -->
+
+<?php
+include('./includes/footer.php');
